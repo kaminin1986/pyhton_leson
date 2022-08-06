8e8152f84e9db098622e818f79613ab7fcaff16c
kaminin1986 <al.kaminin@gmail.com>
2022-08-06 14:06:33 +0300

    много нового

--- tele2bot/bot.py
+++ tele2bot/bot.py
@@ -1,13 +1,14 @@
 import telebot
-import configure
+#import configure
+token = '5144350462:AAGQk2RuOlaPxL5-OSHCgUjHxivm1WKpZhU'
 
-client = telebot.TeleBot(configure.config['token'])
+client = telebot.TeleBot(token)
 
-@client.message_handler(content_types=["text"])
+@client.message_handler(content_types=['text'])
 def get_text(message):
     if message.text.lower() == 'привет':
         client.send_message(message.chat.id, 'Привет, неизвестный пользователь!')
 
 
 
-client.polling(none_stop = True, intrval = 0)
+#client.polling(none_stop = True, intrval = 0)
