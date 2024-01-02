3481b08faa67d2b60702661aa8b9fa90fb1c1d70
alekx <kaminin-a@mail.ru>
2024-01-02 15:51:36 +0300

    lovk

--- main copy.py
+++ main copy.py
@@ -1,4 +1,9 @@
+# importing the eel library 
 import eel
+
+
+
+#print(eel)
 
 eel.init("web")
 eel.start("main.html", size =(400, 400))
---
+++ webse/main.py
@@ -0,0 +1,11 @@
+# importing the eel library 
+import eel
+import bottle.ext.websocket as wbs
+
+#wds
+
+
+#print(eel)
+
+eel.init("web")
+eel.start("main.html", size =(600, 600))
